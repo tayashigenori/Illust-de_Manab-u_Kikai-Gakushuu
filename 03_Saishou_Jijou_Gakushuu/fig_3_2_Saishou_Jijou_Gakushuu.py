715c1599bf53a755033f07112ce9145e66a2c7dc
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-23 21:02:05 +0900

    fix Least Square Learning working!!

--- 03_Saishou_Jijou_Gakushuu/fig_3_2_Saishou_Jijou_Gakushuu.py
+++ 03_Saishou_Jijou_Gakushuu/fig_3_2_Saishou_Jijou_Gakushuu.py
@@ -2,45 +2,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Saishou_Jijou_Gakushuu_tmp:
+    def __init__(self,
+                 X = [0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99],
+                 t = [0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.89, -0.79, -0.04],
+                 phi = lambda x: [1, x, x**2, x**3]):
+        self._X = np.array(X)
+        self._t = np.array(t)
+        self._phi = phi
+        return
+    def get_phi_array(self, X):
+        return np.array([self._phi(x) for x in X])
+    def get_w(self, PHI, t):
+        return np.dot(np.linalg.inv(np.dot(PHI.T, PHI)), np.dot(PHI.T, t))
+    def f(self, w, x):
+        return np.dot(w, self._phi(x))
+
+    def learn(self,):
+        PHI = self.get_phi_array(self._X)
+        return self.get_w(PHI, self._t)
+    def process(self, w):
+        #xlist = np.arange(0, 1, 0.01)
+        xlist = np.arange(-3, 3, 0.1)
+        ylist = [self.f(w, x) for x in xlist]
+        plt.plot(xlist, ylist)
+        plt.show()
+
 class Saishou_Jijou_Gakushuu:
     def __init__(self,):
         return
 
     def get_x(self, n, lower = -3, upper = 3):
         return np.linspace(lower, upper, n)
-    def get_X(self, N, lower = -3, upper = 3):
-        return np.linspace(lower, upper, N)
+    def get_X(self, large_N, lower = -3, upper = 3):
+        return np.linspace(lower, upper, large_N)
     def get_pix(self, x):
         return np.pi * x
     def get_y(self, pix, x, n):
-        return np.sin(pix) / pix + 0.1*x + 0.05 + np.random.randn(n, 1)
+        # y = sin(pix)./(pix) + 0.1*x + 0.05*randn(n,1)
+        return np.sin(pix) / (pix) + 0.1*x + 0.05 * np.random.randn(n)
 
     def initialize_p(self, n, iteration):
         p = np.zeros( (n, 2*iteration) )
         # p(:,1) = ones(n, 1) # n*1
-        p[:,1] = np.ones(n)
+        p[:,0] = np.ones(n)
         return p
-    def initialize_P(self, N, iteration):
-        P = np.zeros( (N, 2*iteration) )
+    def initialize_P(self, large_N, iteration):
+        P = np.zeros( (large_N, 2*iteration) )
         # p(:,1) = ones(N, 1) # N*1
-        P[:,1] = np.ones(N)
+        P[:,0] = np.ones(large_N)
         return P
     def append_p(self, p, j, x):
         p[:, 2*j]   = np.sin(j/2*x)
         p[:, 2*j+1] = np.cos(j/2*x)
         return p
-    def append_P(self, P, j, X):
-        P[:, 2*j]   = np.sin(j/2*X)
-        P[:, 2*j+1] = np.cos(j/2*X)
+    def append_P(self, P, j, large_X):
+        P[:, 2*j]   = np.sin(j/2*large_X)
+        P[:, 2*j+1] = np.cos(j/2*large_X)
         return P
 
     def get_t(self, p, y):
-        return p / y
-    def get_F(self, P, t):
-        return P * t
+        x,resid,rank,s = np.linalg.lstsq(p, y)
+        return x
+    def get_F(self, large_P, t):
+        return np.dot(large_P, t)
 
-    def plot(self, x, y):
-        plt.plot(x, y)
+    def plot(self, large_X, large_F, x, y):
+        plt.plot(large_X, large_F)
+        plt.plot(x, y, 'o')
+        plt.show()
         return
 
     def process(self, n, large_N, iteration):
@@ -59,13 +89,24 @@ class Saishou_Jijou_Gakushuu:
         t = self.get_t(p, y)
         large_F = self.get_F(large_P, t)
 
-        self.plot(large_X, large_F)
-        self.plot(x,y)
+        # plot
+        self.plot(large_X, large_F, x, y)
         return
 
 def main():
     SJG = Saishou_Jijou_Gakushuu()
     SJG.process(n=50, large_N=1000, iteration=15)
+    #n = 50
+    #x = np.linspace(-3, 3, n)
+    #t = np.sin(np.pi * x) / (np.pi * x) + 0.1*x + 0.05 * np.random.randn()
+    #phi = lambda x: [1, np.sin(x/2), np.cos(x/2),
+    #                 np.sin(2*x/2), np.cos(2*x/2),
+    #                 np.sin(3*x/2), np.cos(3*x/2),
+    #                 np.sin(4*x/2), np.cos(4*x/2),
+    #                 np.sin(5*x/2), np.cos(5*x/2)]
+    #SJG = Saishou_Jijou_Gakushuu_tmp(x,t, phi)
+    #w = SJG.learn()
+    #SJG.process(w)
 
 if __name__ == '__main__':
     main()
