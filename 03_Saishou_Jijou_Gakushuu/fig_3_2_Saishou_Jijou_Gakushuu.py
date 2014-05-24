ebe00403b476f03612aaf939d5e6ecd634fe59f7
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-24 23:27:00 +0900

    add gihyo version to Least Square Learning

--- 03_Saishou_Jijou_Gakushuu/fig_3_2_Saishou_Jijou_Gakushuu.py
+++ 03_Saishou_Jijou_Gakushuu/fig_3_2_Saishou_Jijou_Gakushuu.py
@@ -3,54 +3,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #http://gihyo.jp/dev/serial/01/machine-learning/0011
-class Saishou_Jijou_Gakushuu_tmp:
+class Saishou_Jijou_Gakushuu_Gihyo:
     def __init__(self,
-                 X = [0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99],
-                 t = [0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.89, -0.79, -0.04],
                  phi = lambda x: [1, x, x**2, x**3]):
-        self._X = np.array(X)
-        self._t = np.array(t)
         self._phi = phi
+        self._plt = plt
         return
+    """
+    functions for learning
+    """
     def get_phi_array(self, X):
         return np.array([self._phi(x) for x in X])
     def get_w(self, PHI, t):
         return np.dot(np.linalg.inv(np.dot(PHI.T, PHI)), np.dot(PHI.T, t))
+    def learn(self,
+              X = np.array([0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99]),
+              t = np.array([0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.89, -0.79, -0.04])):
+        self._plt.plot(X, t, 'o')
+        PHI = self.get_phi_array(X)
+        w = self.get_w(PHI, t)
+        return w
+    """
+    functions for regression
+    """
     def f(self, w, x):
         return np.dot(w, self._phi(x))
-
-    def learn(self,):
-        PHI = self.get_phi_array(self._X)
-        return self.get_w(PHI, self._t)
-    def process(self, w):
-        #xlist = np.arange(0, 1, 0.01)
-        xlist = np.arange(-3, 3, 0.1)
+    def regression(self, w, xlist = np.arange(0, 1, 0.01)):
         ylist = [self.f(w, x) for x in xlist]
-        plt.plot(xlist, ylist)
-        plt.show()
+        self._plt.plot(xlist, ylist)
+        return ylist
+    def show_plot(self,):
+        self._plt.show()
 
 class Saishou_Jijou_Gakushuu:
-    def __init__(self,):
+    def __init__(self, lower = -3, upper = 3):
+        self._lower = lower
+        self._upper = upper
         return
 
-    def get_x(self, n, lower = -3, upper = 3):
-        return np.linspace(lower, upper, n)
-    def get_large_X(self, large_N, lower = -3, upper = 3):
-        return np.linspace(lower, upper, large_N)
+    def get_x(self, n):
+        return np.linspace(self._lower, self._upper, n)
+    def get_large_X(self, large_N):
+        return np.linspace(self._lower, self._upper, large_N)
     def get_pix(self, x):
         return np.pi * x
     def get_y(self, pix, x, n):
-        # y = sin(pix)./(pix) + 0.1*x + 0.05*randn(n,1)
+        # Matlab: y = sin(pix)./(pix) + 0.1*x + 0.05*randn(n,1)
         return np.sin(pix) / (pix) + 0.1*x + 0.05 * np.random.randn(n)
 
     def initialize_p(self, n, iteration):
         p = np.zeros( (n, 2*iteration) )
-        # p(:,1) = ones(n, 1) # n*1
+        # Matlab: p(:,1) = ones(n, 1) # n*1
         p[:,0] = np.ones(n)
         return p
     def initialize_large_P(self, large_N, iteration):
         P = np.zeros( (large_N, 2*iteration) )
-        # p(:,1) = ones(N, 1) # N*1
+        # Matlab: p(:,1) = ones(N, 1) # N*1
         P[:,0] = np.ones(large_N)
         return P
     def append_p(self, p, j, x):
@@ -95,20 +103,44 @@ class Saishou_Jijou_Gakushuu:
         self.plot(large_X, large_F, x, y)
         return
 
-def main():
+def process_gihyo_with_default_data():
+    # initialize with phi
+    SJG = Saishou_Jijou_Gakushuu_Gihyo()
+    # learn with default training data
+    w = SJG.learn()
+    # regression
+    ylist = SJG.regression(w)
+    SJG.show_plot()
+def process_gihyo(test_mode = False):
+    if test_mode == True:
+        process_gihyo_with_default_data()
+        return
+    phi = lambda x: [1, np.sin(x/2), np.cos(x/2),
+                     np.sin(2*x/2), np.cos(2*x/2),
+                     np.sin(3*x/2), np.cos(3*x/2),
+                     np.sin(4*x/2), np.cos(4*x/2),
+                     np.sin(5*x/2), np.cos(5*x/2)] # TODO: extend this list to [np.sin(15*x/2), np.cos(15*x/2)]
+    # initialize with phi
+    SJG = Saishou_Jijou_Gakushuu_Gihyo(phi)
+    # training data
+    n = 50
+    X = np.linspace(-3, 3, n)
+    t = np.sin(np.pi * X) / (np.pi * X) + 0.1*X + 0.05 * np.random.randn()
+    # learn
+    w = SJG.learn(X, t)
+    # regression
+    N = 1000
+    xlist = np.linspace(-3, 3, N)
+    ylist = SJG.regression(w, xlist)
+    SJG.show_plot()
+def process():
     SJG = Saishou_Jijou_Gakushuu()
     SJG.process(n=50, large_N=1000, iteration=15)
-    #n = 50
-    #x = np.linspace(-3, 3, n)
-    #t = np.sin(np.pi * x) / (np.pi * x) + 0.1*x + 0.05 * np.random.randn()
-    #phi = lambda x: [1, np.sin(x/2), np.cos(x/2),
-    #                 np.sin(2*x/2), np.cos(2*x/2),
-    #                 np.sin(3*x/2), np.cos(3*x/2),
-    #                 np.sin(4*x/2), np.cos(4*x/2),
-    #                 np.sin(5*x/2), np.cos(5*x/2)]
-    #SJG = Saishou_Jijou_Gakushuu_tmp(x,t, phi)
-    #w = SJG.learn()
-    #SJG.process(w)
+def main(use_gihyo_version = False):
+    if use_gihyo_version:
+        process_gihyo()
+    else:
+        process()
 
 if __name__ == '__main__':
     main()
